acd4c4b24b7b354c0e00a6fcbefa9a3d6ecbb9d4
unarix <team@unarix.com.ar>
2020-05-11 20:49:12 -0300

    Add: Init

---
+++ uMouseMove/frmTimer.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace uMouseMove
+{
+    partial class frmTimer
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnInit = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnInit
+            // 
+            this.btnInit.Location = new System.Drawing.Point(12, 12);
+            this.btnInit.Name = "btnInit";
+            this.btnInit.Size = new System.Drawing.Size(75, 23);
+            this.btnInit.TabIndex = 0;
+            this.btnInit.Text = "Iniciar";
+            this.btnInit.UseVisualStyleBackColor = true;
+            this.btnInit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(93, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Parar";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cuidado: Hace click en el top 0. ";
+            // 
+            // frmTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(181, 58);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnInit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MinimizeBox = false;
+            this.Name = "frmTimer";
+            this.Opacity = 0.8D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "uMouseMove";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInit;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ uMouseMove/frmTimer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uMouseMove
+{
+    public partial class frmTimer : Form
+    {
+        public frmTimer()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true; // Arranco el timer
+
+            btnInit.Enabled = false;
+            btnStop.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false; // Freno el timer
+
+            btnInit.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void MoveCursor()
+        {
+            Random r = new Random();
+            int rInt = r.Next(1000,2000); //for ints
+            int range = 150;
+            double dPosicion = r.NextDouble() * range;
+            int iPosicion = (int)dPosicion;
+
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+
+            if (Cursor.Position.Y == 0)
+            {
+                //Cursor.Position = new Point(iPosicion, 0);
+                uMouseMove.MouseOperations.SetCursorPosition(iPosicion, 0);
+                uMouseMove.MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
+                uMouseMove.MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MoveCursor();
+        }
+
+    }
+}
